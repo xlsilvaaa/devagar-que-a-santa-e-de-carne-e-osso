a1da34bf742153f72cffffa10dd12aa65f1e5f1d
xlsilvaaa <giselesilva080792@gmail.com>
2023-03-06 13:28:41 -0300

    exercícios de fixação do módulo I

---
+++ concatenação.js
@@ -0,0 +1,31 @@
+/* 
+EXISTEM DUAS FORMAS DE CONCATENAÇÃO, SENDO ELAS A TRADICIONAL E O TEMPLATE STRING:
+> Tradicional - console.log("string " + variável + " texto " + variável + " string!");
+
+> Template String - PRESTAR ATENÇÃO QUE SE USA CRASE E NÃO ASPAS. 
+console.log(` string ${variável} string ${variável} string!`);
+*/
+
+/*
+Pratica 3: Crie as seguintes constantes e em seguida use a criatividade para concatená-las, 
+formando uma frase: primeiro nome; último nome; ano de nascimento; cor preferida; hobby; um sentimento.
+const primeiroNome = "Gisele";
+const ultimoNome = "Silva";
+const anoDeNascimento = "1992";
+const corPreferida = "Amarelo";
+const hobby = "andar de bike";
+const umSentimento = "confusa e estressada";
+*/
+//console.log("E aeeee Braseeel! Me chamo " + primeiroNome + ultimoNome + "nasci em" + anoDeNascimento + ". Minha cor favorita é o" + corPreferida + "e tenho um hobby que é" + hobby + ", pois às vezes me sinto muito" + umSentimento + "então isso acaba me distraindo!"); 
+
+//console.log(`E aee braseel! Me chamo ${primeiroNome} ${ultimoNome} e nasci em ${anoDeNascimento}. Minha cor favorita é o ${corPreferida}, e tenho um hobby que é ${hobby}, pois às vezes me sinto muito ${umSentimento}, e aí ${hobby} acaba me distraindo!`);
+
+/*
+EXERCÍCIO EXTRA PARA FIXAR - Declare e insira os valores nas seguintes variáveis ou constantes:
+const nomeDoPais = "Brasíl";
+let pontosTuristicos = "Marco Zero, Buraco da véa e a minha casa";
+const capitalDoPais = "Brasília - DF";
+const quantidadeDeEstados = 27;
+const paisDaAmericaLatina = "Sulamericanos";
+*/
+//console.log(`Oii gente! Moro num país tropical abençoado pelas deusas chamado ${nomeDoPais}, cuja capital é ${capitalDoPais} e temos um total de ${quantidadeDeEstados}. Eu moro na região nordeste e aqui temos muita coisa massa pra conhecer como por exemplo o ${pontosTuristicos}. Só tenho a agradecer por termos nascido ${paisDaAmericaLatina}.`);
---
+++ declarandoVariáveisEConstantes.js
@@ -0,0 +1,25 @@
+/*Pratica 1: crie 3 constantes e mostre no console o valor salvo nelas
+const um = "Amarelo";
+const dois = "Laranja";
+const tres = "Vermelho";
+console.log(um)
+console.log(dois)
+console.log(tres)*/
+
+/*Pratica 2: Declare e insira os valores nas seguintes variáveis ou constantes: Nome do país, Pontos turísticos,
+Capital do país, Quantidade de estados, O país é da América Latina?
+const nomePais = "Brasil";
+let pontosTurísticos = "Marco Zero, Buraco da véa, Armazém do Campo, Minha casa";
+const capitalPais = "Brasília - DF";
+const quantidadeEstados = 27;
+const paisAmericaLatina = "Sulamericanos";
+console.log(nomePais);
+console.log(pontosTurísticos);
+console.log(capitalPais);
+console.log(quantidadeEstados);
+console.log(paisAmericaLatina);
+
+let pontosTuristicos = "Mercado Ver-o-peso";
+console.log(pontosTuristicos)*/
+
+
